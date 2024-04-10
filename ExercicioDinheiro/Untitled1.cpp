d57fe4d60152b972298f4698203b02fc666c336b
João Vitor Seiji Yoneyama <jvsyoneyama@gmail.com>
2024-04-10 13:20:10 -0300

    Testando

--- ExercicioDinheiro/Untitled1.cpp
+++ ExercicioDinheiro/Untitled1.cpp
@@ -6,19 +6,43 @@
 			
 			setlocale(LC_ALL, "Portuguese");
 			
-			float valor, m05;
-			int moedas05 = 0;
+			float valor, m05, m10, m25, m50, m1;
+			int moeda05=0, moeda10=0, moeda25=0, moeda50=0, moeda1=0;
 			
 			printf("\nInsira o valor desejado: ");
 			scanf("%f", &valor);
 			
-			for(m05=0; m05<valor; m05=m05+0.05){			
-						
-				moedas05++;					
-						
+			for(m05=0.05; m05<=valor+0.001; m05=m05+0.05){ //valor+0.001 pq a soma de vários 0.05 causa imprecisão no cálculo
+				
+				moeda05++;
+		
 			}
 			
-			printf("\nMoedas de 5 centavos: %d", moedas05);
+			for(m10=0.1; m10<=valor+0.001; m10=m10+0.1){
+					
+				moeda10++;
+					
+			}
+			
+			for(m25=0.25; m25<=valor; m25=m25+0.25){
+				
+				moeda25++;
+				
+			}
+			
+			for(m50=0.5; m50<=valor; m50=m50+0.5){
+				
+				moeda50++;
+				
+			}
+			
+			for(m1=1; m1<=valor; m1++){
+				
+				moeda1++;
+				
+			}
+			
+			printf(" moedas de 0.05: %d - moedas de 0.1: %d - moedas de 0.25: %d - moedas de 0.5: %d - moedas de 1: %d", moeda05, moeda10, moeda25, moeda50, moeda1);
 			
 			return 0;
 		}
