6b49e824fcd980cb074bd1c90be0bcd04436c6d2
João Vitor Seiji Yoneyama <jvsyoneyama@gmail.com>
2024-04-12 11:32:04 -0300

    versão melhorada do exercício

--- ExercicioDinheiro/Untitled1.cpp
+++ ExercicioDinheiro/Untitled1.cpp
@@ -1,4 +1,4 @@
-		#include<stdio.h>
+		#include<stdio.h> 
 		#include<stdlib.h>
 		#include<locale.h>
 		
@@ -7,12 +7,12 @@
 			setlocale(LC_ALL, "Portuguese");
 			
 			float valor, m05, m10, m25, m50, m1;
-			int moeda05=0, moeda10=0, moeda25=0, moeda50=0, moeda1=0;
+			int moeda05=0, moeda10=0, moeda25=0, moeda50=0, moeda1=0, possib=0;
 			
 			printf("\nInsira o valor desejado: ");
 			scanf("%f", &valor);
 			
-			for(m05=0.05; m05<=valor+0.001; m05=m05+0.05){ //valor+0.001 pq a soma de vários 0.05 causa imprecisão no cálculo
+			/*for(m05=0.05; m05<=valor+0.001; m05=m05+0.05){ //valor+0.001 pq a soma de vÃ¡rios 0.05 causa imprecisÃ£o no cÃ¡lculo
 				
 				moeda05++;
 		
@@ -42,7 +42,82 @@
 				
 			}
 			
-			printf(" moedas de 0.05: %d - moedas de 0.1: %d - moedas de 0.25: %d - moedas de 0.5: %d - moedas de 1: %d", moeda05, moeda10, moeda25, moeda50, moeda1);
+			printf(" moedas de 0.05: %d - moedas de 0.1: %d - moedas de 0.25: %d - moedas de 0.5: %d - moedas de 1: %d", moeda05, moeda10, moeda25, moeda50, moeda1);*/
+			
+			if(valor>=1) for(m1=0; m1<=valor; m1++){
+			
+				for(m50=0; m50<=valor; m50=m50+0.5){
+				
+					for(m25=0; m25<=valor; m25=m25+0.25){
+						
+						for(m10=0; m10<=valor+0.001; m10=m10+0.1){
+							
+							for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+								
+								if(valor==m05+m10+m25+m50+m1) possib++;
+								
+							}
+						}
+					}
+				}
+		
+			}else if(valor>=0.5 && valor<1){
+				
+				for(m50=0; m50<=valor; m50=m50+0.5){
+				
+					for(m25=0; m25<=valor; m25=m25+0.25){
+						
+						for(m10=0; m10<=valor+0.001; m10=m10+0.1){
+							
+							for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+								
+								if(valor==m05+m10+m25+m50) possib++;
+								
+							}
+						}
+					}
+				}		
+		
+			}else if(valor>=0.25 && valor<0.5){
+				
+				for(m25=0; m25<=valor; m25=m25+0.25){
+						
+					for(m10=0; m10<=valor+0.001; m10=m10+0.1){
+						
+						for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+							
+							if(valor==m05+m10+m25) possib++;
+								
+						}
+					}
+				}
+			
+			}else if(valor>=0.1 && valor<0.25){
+				
+				for(m10=0; m10<=valor+0.001; m10=m10+0.1){
+					
+					for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+							
+						if(valor==m05+m10) possib++;
+								
+					}
+				}
+		
+			}else if(valor>=0.05 && valor<0.1){
+				
+				for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+							
+					if(valor==m05) possib++;
+								
+				}
+		
+			}else{
+				
+				printf("Valor muito pequeno");
+				
+			}
+			
+			printf("%d", possib);
 			
 			return 0;
 		}
