f15f8e34a6f5f5d32d747c87bad246b620f98e21
JoYoneyama <jvsyoneyama@gmail.com>
2024-04-13 12:58:39 -0300

    ta certo, mas n ta

--- ExercicioDinheiro/Untitled1.cpp
+++ ExercicioDinheiro/Untitled1.cpp
@@ -6,63 +6,35 @@
 			
 			setlocale(LC_ALL, "Portuguese");
 			
-			float valor, m05, m10, m25, m50, m1;
 			int moeda05=0, moeda10=0, moeda25=0, moeda50=0, moeda1=0, possib=0;
+			float m05, m10, m25, m50, m1, valor;
 			
-			printf("\nInsira o valor desejado: ");
+			printf("Digite um valor: R$");
 			scanf("%f", &valor);
 			
-			/*for(m05=0.05; m05<=valor+0.001; m05=m05+0.05){   //valor+0.001 pq a soma de vários 0.05 causa imprecisão no cálculo
+			for(m05=0; m05 <= valor+0.001; m05=m05+0.05){
 				
-				moeda05++;
-		
-			}
-			
-			for(m10=0.1; m10<=valor+0.001; m10=m10+0.1){
+				for(m10=0; m05+m10 <= valor+0.001; m10=m10+0.1){
 					
-				moeda10++;
-					
-			}
-			
-			for(m25=0.25; m25<=valor; m25=m25+0.25){
-				
-				moeda25++;
-				
-			}
-			
-			for(m50=0.5; m50<=valor; m50=m50+0.5){
-				
-				moeda50++;
-				
-			}
-			
-			for(m1=1; m1<=valor; m1++){
-				
-				moeda1++;
-				
-			}
-			
-			printf(" moedas de 0.05: %d - moedas de 0.1: %d - moedas de 0.25: %d - moedas de 0.5: %d - moedas de 1: %d", moeda05, moeda10, moeda25, moeda50, moeda1);*/
-			
-			for(m1=0; m1<=valor; m1++){
-			
-				for(m50=0; m50<=valor; m50=m50+0.5){
-				
-					for(m25=0; m25<=valor; m25=m25+0.25){
+					for(m25=0; m05+m10+m25 <= valor; m25=m25+0.25){
 						
-						for(m10=0; m10<=valor+0.001; m10=m10+0.1){
+						for(m50=0; m05+m10+m25+m50 <= valor; m50=m50+0.5){
 							
-							for(m05=0; m05<=valor+0.001; m05=m05+0.05){
+							for(m1=0; m05+m10+m25+m50+m1 <= valor; m1++){
 								
-								if(valor==m05+m10+m25+m50+m1) possib++;
-								
+								if(valor == m1+m50+m25+m10+m05){
+									
+									possib++;
+									
+									printf("\n%d. R$1,00: %.2f - R$0,50: %.2f - R$0,25: %.2f - R$0,10: %.2f - R$0,05: %.2f\n", possib, m1, m50, m25, m10, m05);	
+									
+								}
+
 							}
 						}
 					}
 				}
 			}
-			
-			printf("%d", possib);
 			
 			return 0;
 		}
