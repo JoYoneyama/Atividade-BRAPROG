9685d9d73b6e265d42371b747c29b01237539d70
JoYoneyama <jvsyoneyama@gmail.com>
2024-05-02 17:34:56 -0300

    Adicionada as funções

--- ExercicioMenu/Untitled1.cpp
+++ ExercicioMenu/Untitled1.cpp
@@ -3,6 +3,98 @@
 	#include<locale.h>
 	#include<cstdlib>  // adiciona o pressione qualquer tecla para sair
 	
+	void listaPedido1(){
+		
+		printf("Você ainda não fez nenhum pedido\n\n");
+				
+		system("pause");
+		system("cls");
+		
+	}
+	
+	void listaPedido2(float valor){
+		
+		printf("Pedidos:\n\n");
+		printf("Total: R$%.2f\n\n", valor);
+		system("pause");
+		system("cls");
+		
+	}
+	
+	void finalizar1(int sair){
+		
+		printf("\nVocê ainda não fez nenhum pedido. Tem certeza?\n");
+		printf("1. Sim\n2. Não");
+		printf("\n\nNúmero: ");
+		scanf("%d", &sair);
+					
+		system("cls");
+					
+		while(sair != 1 && sair != 2){
+					
+			printf("\nVocê ainda não fez nenhum pedido. Tem certeza?\n");
+			printf("1. Sim\n2. Não");
+			printf("\n\nDigite um código válido\n");
+			printf("\nNúmero: ");
+			scanf("%d", &sair);
+						
+			system("cls");
+						
+		}
+							
+		if(sair == 1){
+						
+			system("cls");
+			printf("Finalizando...");
+						
+			exit(0);
+						
+		}else if(sair == 2){
+						
+			system("cls");
+						
+		}	
+			
+	}
+	
+	void finalizar2(int sair, float valor){
+		
+		printf("\nFinalizando pedido. Tem certeza?\n");
+		printf("1. Sim\n2. Não");
+		printf("\n\nNúmero: ");
+		scanf("%d", &sair);
+						
+		system("cls");
+					
+		while(sair != 1 && sair != 2){
+						
+			printf("\nFinalizando pedido. Tem certeza?\n");
+			printf("1. Sim\n2. Não");
+			printf("\n\nDigite um código válido\n");	
+			printf("\nNúmero: ");
+			scanf("%d", &sair);	
+						
+			system("cls");
+						
+		}
+					
+		if(sair == 1){
+						
+			system("cls");	
+						
+			printf("Finalizando...\n");
+			printf("Total a ser pago: R$%.2f", valor);
+						
+			exit(0);
+						
+		}else if(sair == 2){
+						
+			system("cls");
+						
+		}
+		
+	}
+	
 	int main(){
 		
 		setlocale(LC_ALL, "Portuguese");
@@ -10,93 +102,70 @@
 		int codigo, sair;
 		float valor;
 		
-		printf("MENU\n");
-		printf("------------------------------\n");
-		printf("Faça seu pedido: \n");
-		printf("1. Sanduíche natural - R$5,00\n2. X-Salada - R$15,00\n3. Refrigerante - R$3,50\n4. Suco - R$3,00\n5. Sorvete - R$9,00\n6. Lista de pedidos\n7. Finalizar pedido\n\n");
-		printf("Número: ");
-		scanf("%d", &codigo);
+		do{
 		
-		system("cls");
-		
-		while(codigo < 1 || codigo >7){
-			
 			printf("MENU\n");
 			printf("------------------------------\n");
 			printf("Faça seu pedido: \n");
 			printf("1. Sanduíche natural - R$5,00\n2. X-Salada - R$15,00\n3. Refrigerante - R$3,50\n4. Suco - R$3,00\n5. Sorvete - R$9,00\n6. Lista de pedidos\n7. Finalizar pedido\n\n");
-			printf("Digite um código válido\n\n");
 			printf("Número: ");
 			scanf("%d", &codigo);
 		
 			system("cls");
-			
-		}
 		
-		switch(codigo){
+			while(codigo < 1 || codigo > 7){
 			
-			case 1:
-				valor = valor + 5;
-				break;
-			
-			case 2:
-				valor = valor + 15;
-				break;
-				
-			case 3:
-				valor = valor + 3.5;
-				break;
-				
-			case 4:
-				valor = valor + 3;
-				break;
-				
-			case 5:
-				valor = valor + 9;
-				break;
-				
-			case 6:
-				printf("Você ainda não fez nenhum pedido\n\n");
-				system("pause");
+				printf("MENU\n");
+				printf("------------------------------\n");
+				printf("Faça seu pedido: \n");
+				printf("1. Sanduíche natural - R$5,00\n2. X-Salada - R$15,00\n3. Refrigerante - R$3,50\n4. Suco - R$3,00\n5. Sorvete - R$9,00\n6. Lista de pedidos\n7. Finalizar pedido\n\n");
+				printf("Digite um código válido\n\n");
+				printf("Número: ");
+				scanf("%d", &codigo);
+		
 				system("cls");
-				break;
+			
+			}
+		
+			switch(codigo){
+			
+				case 1:
+					valor = valor + 5;
+					break;
+			
+				case 2:
+					valor = valor + 15;
+					break;
 				
-			case 7:
-				do{
-					
-					printf("\nVocê ainda não fez nenhum pedido. Tem certeza?\n");
-					printf("1. Sim\n2. Não");
-					printf("\n\nNúmero: ");
-					scanf("%d", &sair);
-					
-					if(sair == 1){
-						
-						system("cls");
-						printf("Finalizando...");
-						
-						return 0;
-						
-					}else if(sair == 2){
-						
-						system("cls");
-						break;
-						
-					}else{
-						
-						system("cls");
-						
-						printf("\nDigite um código válido\n");
-						
-					}
-					
-				}while(sair != 1 || sair != 2);					
-				break;
+				case 3:
+					valor = valor + 3.5;
+					break;
 				
-		}
+				case 4:
+					valor = valor + 3;
+					break;
+				
+				case 5:
+					valor = valor + 9;
+					break;
+					
+				case 6:
+					listaPedido1();
+					break;
+				
+				case 7:
+					finalizar1(sair);		
+					break;
+				
+			}
+			
+		}while(valor == 0);
 		
 		do{
 			
-			printf("\nAdicionar ao pedido: \n");
+			printf("MENU\n");
+			printf("------------------------------\n");
+			printf("Adicionar ao pedido: \n");
 			printf("1. Sanduíche natural - R$5,00\n2. X-Salada - R$15,00\n3. Refrigerante - R$3,50\n4. Suco - R$3,00\n5. Sorvete - R$9,00\n6. Lista de pedidos\n7. Finalizar pedido\n\n");
 			printf("Número: ");
 			scanf("%d", &codigo);
@@ -105,7 +174,9 @@
 			
 			while(codigo < 1 || codigo > 7){
 				
-				printf("\nAdicionar ao pedido: \n");
+				printf("MENU\n");
+				printf("------------------------------\n");
+				printf("Adicionar ao pedido: \n");
 				printf("1. Sanduíche natural - R$5,00\n2. X-Salada - R$15,00\n3. Refrigerante - R$3,50\n4. Suco - R$3,00\n5. Sorvete - R$9,00\n6. Lista de pedidos\n7. Finalizar pedido\n\n");
 				printf("digite um número válido\n\n");
 				printf("Número: ");
@@ -138,46 +209,11 @@
 					break;
 				
 				case 6:
-					printf("Pedidos:\n\n");
-					printf("Total: R$%.2f\n\n", valor);
-					system("pause");
-					system("cls");
+					listaPedido2(valor);
 					break;
 					
-				case 7:
-					do{			
-					
-						printf("\nFinalizando pedido. Tem certeza?\n");
-						printf("1. Sim\n2. Não");
-						printf("\n\nNúmero: ");
-						scanf("%d", &sair);
-						
-						system("cls");
-					
-						if(sair == 1){
-						
-							system("cls");
-							printf("Finalizando...\n");
-							printf("Total a ser pago: R$%.2f", valor);
-						
-							return 0;
-						
-						}else if(sair == 2){
-						
-							break;
-						
-						}else{
-						
-							system("cls");
-							
-							printf("Digite um código válido\n");
-						
-						}
-					
-						}while(sair != 1 || sair != 2);					
-						break;
-				
-				default:
+				case 7:		
+					finalizar2(sair, valor);
 					break;
 		
 			}
